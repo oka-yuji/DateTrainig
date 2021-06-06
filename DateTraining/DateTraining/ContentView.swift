98357cabfac77473287b2ddab1bec4f2b87bb396
岡優志 <yu.j.0513@gmail.com>
2021-06-06 23:39:34 +0900

    DateComponentsFormatterを追加

--- DateTraining/DateTraining/ContentView.swift
+++ DateTraining/DateTraining/ContentView.swift
@@ -9,8 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button(action: {
+            let time: TimeInterval = 30000000
+                       let formatter = DateComponentsFormatter()
+                       formatter.unitsStyle = .full
+                       formatter.includesApproximationPhrase = false
+                       formatter.includesTimeRemainingPhrase = false
+                       formatter.allowedUnits = [.day, .hour, .minute, .second]
+                       let outputString = formatter.string(from: time)!
+                       print(outputString)
+        }, label: {
+            Text("Button")
+        })
     }
 }
 
